db964b797e7727000beedf559dae5d7955be42ab
Yukai Huang <yukaihuangtw@gmail.com>
2023-04-28 15:08:11 +0800

    fix: install from head

--- Formula/warpd.rb
+++ Formula/warpd.rb
@@ -4,17 +4,18 @@ class Warpd < Formula
     url "https://github.com/rvaiya/warpd/releases/download/v1.3.5/warpd-1.3.5-osx.tar.gz"
     sha256 "4a4bbaeb3a864cd45b3d3bd841bdd87dc67b2099b860da18b81efb23d28afb2b"
     version "1.3.5"
-    head "https://github.com/rvaiya/warpd.git", branch: "main"
+    head "https://github.com/rvaiya/warpd.git", branch: "master"
   
     depends_on :macos
     depends_on xcode: :build if build.head?
   
     def install
-      if build.head?
+    if build.head?
         system "make"
+        system "make", "man"
         bin.install "warpd"
-        man1.install "warpd.1"
-      else
+        man1.install "files/warpd.1.gz"
+    else
         bin.install "usr/local/bin/warpd"
         man1.install "usr/local/share/man/man1/warpd.1.gz"
       end
