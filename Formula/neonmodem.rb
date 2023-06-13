a0ed3192cc96d8ca85ae3014f0f1254a62e35f6e
Yukai Huang <yukaihuangtw@gmail.com>
2023-06-13 19:02:55 +0800

    Update neonmodem.rb

--- Formula/neonmodem.rb
+++ Formula/neonmodem.rb
@@ -4,8 +4,6 @@ class Neonmodem < Formula
   url "https://github.com/mrusme/neonmodem/releases/download/v1.0.2/neonmodem_1.0.2_darwin_amd64.tar.gz"
   sha256 "4d3875df2bf46b71ce2b8ff11d80851bd4be53c265c950e9fbee1f185cc79b47"
   version "1.0.2"
-  
-  depends_on arch: [:aarch64]
 
   def install
     bin.install "neonmodem"
