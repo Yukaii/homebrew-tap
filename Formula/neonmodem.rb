edaa24eb4ca5dd9781208dcc172af4f2b7ad857d
Yukai Huang <yukaihuangtw@gmail.com>
2023-06-13 19:02:06 +0800

    Create neonmodem.rb

---
+++ Formula/neonmodem.rb
@@ -0,0 +1,17 @@
+class Neonmodem < Formula
+  desc "Neonmodem is a modem for your terminal"
+  homepage "https://github.com/mrusme/neonmodem"
+  url "https://github.com/mrusme/neonmodem/releases/download/v1.0.2/neonmodem_1.0.2_darwin_amd64.tar.gz"
+  sha256 "4d3875df2bf46b71ce2b8ff11d80851bd4be53c265c950e9fbee1f185cc79b47"
+  version "1.0.2"
+  
+  depends_on arch: [:aarch64]
+
+  def install
+    bin.install "neonmodem"
+  end
+
+  test do
+    system "#{bin}/neonmodem", "--version"
+  end
+end
