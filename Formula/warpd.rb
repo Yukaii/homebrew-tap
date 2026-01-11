19759df4f6e701de5849f6962e3589771f464056
Yukai Huang <yukaihuangtw@gmail.com>
2026-01-11 10:44:19 -0800

    chore: update warpd distribution

--- Formula/warpd.rb
+++ Formula/warpd.rb
@@ -1,34 +1,25 @@
 class Warpd < Formula
-    desc "A modal keyboard driven interface for mouse manipulation"
-    homepage "https://github.com/rvaiya/warpd"
-    url "https://github.com/rvaiya/warpd/releases/download/v1.3.5/warpd-1.3.5-osx.tar.gz"
-    sha256 "4a4bbaeb3a864cd45b3d3bd841bdd87dc67b2099b860da18b81efb23d28afb2b"
-    version "1.3.5"
-    head "https://github.com/rvaiya/warpd.git", branch: "master"
-  
-    depends_on :macos
-    depends_on xcode: :build if build.head?
-  
-    def install
-        if build.head?
-            system "make"
-            bin.install "bin/warpd"
-            man1.install "files/warpd.1.gz"
-            (prefix/"Library/LaunchAgents").install "files/com.warpd.warpd.plist"
-        else
-            bin.install "usr/local/bin/warpd"
-            man1.install "usr/local/share/man/man1/warpd.1.gz"
-            (prefix/"Library/LaunchAgents").install "Library/LaunchAgents/com.warpd.warpd.plist"
-        end
-    end
-    
-    service do
-        run [opt_bin/"warpd", "-f"]
-        keep_alive true
-        process_type :interactive
-    end
+  desc 'A modal keyboard driven interface for mouse manipulation'
+  homepage 'https://gitlab.com/B1T3X/warpd'
+  head 'https://gitlab.com/B1T3X/warpd.git', branch: 'master'
 
-    test do
-        system "#{bin}/warpd", "--version"
-    end
+  depends_on :macos
+  depends_on xcode: :build
+
+  def install
+    system 'make'
+    bin.install 'bin/warpd'
+    man1.install 'files/warpd.1.gz'
+    (prefix / 'Library/LaunchAgents').install 'files/com.warpd.warpd.plist'
   end
+
+  service do
+    run [opt_bin / 'warpd', '-f']
+    keep_alive true
+    process_type :interactive
+  end
+
+  test do
+    system "#{bin}/warpd", '--version'
+  end
+end
