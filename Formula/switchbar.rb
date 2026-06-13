9f445e9787cb1f0a0ef5d8458665da2cfee0c85e
Yukai Huang <yukaihuangtw@gmail.com>
2026-06-12 21:29:32 -0600

    fix: use GitHub release binary download

--- Formula/switchbar.rb
+++ Formula/switchbar.rb
@@ -1,21 +1,26 @@
 class Switchbar < Formula
   desc "Quick macOS default browser switcher from the menu bar"
   homepage "https://github.com/Yukaii/Switchbar"
-  url "https://github.com/Yukaii/Switchbar/archive/refs/tags/v#{version}.tar.gz"
   version "1.0.0"
   license "MIT"
+  url "https://github.com/Yukaii/Switchbar/releases/download/v1.0.0/Switchbar-v1.0.0-arm64.tar.gz"
+  sha256 "2836d79c8f39a8d4615f41e2462a983e7f4f6b0a3cb4d171f17cdd62ddd458e2"
 
   head "https://github.com/Yukaii/Switchbar.git", branch: "main"
 
   depends_on :xcode => :build
 
   def install
-    system "swift", "build", "-c", "release"
-    bin.install ".build/release/Switchbar"
+    if build.head?
+      system "swift", "build", "-c", "release"
+      bin.install ".build/release/Switchbar"
+    else
+      bin.install "Switchbar"
+    end
+  end
 
-    # Install launch agent plist for auto-start
-    plist_path = prefix/"com.yukaii.switchbar.plist"
-    plist_path.write <<~PLIST
+  def post_install
+    (prefix/"com.yukaii.switchbar.plist").write <<~PLIST
       <?xml version="1.0" encoding="UTF-8"?>
       <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
       <plist version="1.0">
@@ -35,23 +40,15 @@ class Switchbar < Formula
     PLIST
   end
 
-  def post_install
-    puts <<~EOS
-      To auto-start Switchbar on login:
-
-        cp #{opt_prefix}/com.yukaii.switchbar.plist ~/Library/LaunchAgents/
-        launchctl load ~/Library/LaunchAgents/com.yukaii.switchbar.plist
-
-      Or run it manually:
-
-        Switchbar &
-    EOS
-  end
-
   def caveats
     <<~EOS
       Switchbar runs as a menu bar app. The keyboard shortcut
       (Option+Space by default) opens the browser switcher menu.
+
+      To auto-start on login:
+
+        cp #{opt_prefix}/com.yukaii.switchbar.plist ~/Library/LaunchAgents/
+        launchctl load ~/Library/LaunchAgents/com.yukaii.switchbar.plist
 
       Inspired by Default Browser: https://sindresorhus.com/default-browser
     EOS
